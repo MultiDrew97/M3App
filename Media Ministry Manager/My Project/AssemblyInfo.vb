7b5e1b1b46a2987cb73e1080826f004b8e97494f
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2022-11-10 09:18:46 -0600

    publishing a hot fix

--- Media Ministry Manager/My Project/AssemblyInfo.vb
+++ Media Ministry Manager/My Project/AssemblyInfo.vb
@@ -32,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("3.0.717.10")>
-<Assembly: AssemblyFileVersion("3.0.713.10")>
+<Assembly: AssemblyFileVersion("3.0.717.10")>
 <Assembly: NeutralResourcesLanguage("en-US")>
