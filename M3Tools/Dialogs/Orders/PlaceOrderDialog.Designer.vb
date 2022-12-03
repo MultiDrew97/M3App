75aa3cbbdc0308c820ece49cd35f26199b2bfd6b
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2022-12-03 14:36:34 -0600

    beginnings of place orders custom dialog

--- M3Tools/Dialogs/Orders/PlaceOrderDialog.Designer.vb
+++ M3Tools/Dialogs/Orders/PlaceOrderDialog.Designer.vb
@@ -22,10 +22,18 @@ Partial Class PlaceOrderDialog
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
 		Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-		Me.OK_Button = New System.Windows.Forms.Button()
-		Me.Cancel_Button = New System.Windows.Forms.Button()
+		Me.btn_Checkout = New System.Windows.Forms.Button()
+		Me.btn_Cancel = New System.Windows.Forms.Button()
+		Me.ccb_Customers = New SPPBC.M3Tools.CustomersComboBox()
 		Me.pcb_Items = New SPPBC.M3Tools.ProductsComboBox()
+		Me.QuantityNudCtrl1 = New SPPBC.M3Tools.QuantityNudCtrl()
+		Me.lbl_Total = New System.Windows.Forms.Label()
+		Me.txt_Total = New System.Windows.Forms.TextBox()
+		Me.btn_AddCart = New System.Windows.Forms.Button()
+		Me.OrdersDatabase1 = New SPPBC.M3Tools.Database.OrdersDatabase(Me.components)
+		Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
 		Me.TableLayoutPanel1.SuspendLayout()
 		Me.SuspendLayout()
 		'
@@ -35,54 +43,111 @@ Partial Class PlaceOrderDialog
 		Me.TableLayoutPanel1.ColumnCount = 2
 		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
 		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-		Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
-		Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
-		Me.TableLayoutPanel1.Location = New System.Drawing.Point(277, 274)
+		Me.TableLayoutPanel1.Controls.Add(Me.btn_Checkout, 0, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.btn_Cancel, 1, 0)
+		Me.TableLayoutPanel1.Location = New System.Drawing.Point(148, 312)
 		Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
 		Me.TableLayoutPanel1.RowCount = 1
 		Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
 		Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
 		Me.TableLayoutPanel1.TabIndex = 0
 		'
-		'OK_Button
+		'btn_Checkout
 		'
-		Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-		Me.OK_Button.Location = New System.Drawing.Point(3, 3)
-		Me.OK_Button.Name = "OK_Button"
-		Me.OK_Button.Size = New System.Drawing.Size(67, 23)
-		Me.OK_Button.TabIndex = 0
-		Me.OK_Button.Text = "OK"
+		Me.btn_Checkout.Anchor = System.Windows.Forms.AnchorStyles.None
+		Me.btn_Checkout.Location = New System.Drawing.Point(3, 3)
+		Me.btn_Checkout.Name = "btn_Checkout"
+		Me.btn_Checkout.Size = New System.Drawing.Size(67, 23)
+		Me.btn_Checkout.TabIndex = 0
+		Me.btn_Checkout.Text = "Checkout"
 		'
-		'Cancel_Button
+		'btn_Cancel
 		'
-		Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-		Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-		Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
-		Me.Cancel_Button.Name = "Cancel_Button"
-		Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
-		Me.Cancel_Button.TabIndex = 1
-		Me.Cancel_Button.Text = "Cancel"
+		Me.btn_Cancel.Anchor = System.Windows.Forms.AnchorStyles.None
+		Me.btn_Cancel.Location = New System.Drawing.Point(76, 3)
+		Me.btn_Cancel.Name = "btn_Cancel"
+		Me.btn_Cancel.Size = New System.Drawing.Size(67, 23)
+		Me.btn_Cancel.TabIndex = 1
+		Me.btn_Cancel.Text = "Cancel"
 		'
-		'ProductsComboBox1
+		'ccb_Customers
+		'
+		Me.ccb_Customers.AutoSize = True
+		Me.ccb_Customers.Location = New System.Drawing.Point(42, 12)
+		Me.ccb_Customers.MaximumSize = New System.Drawing.Size(0, 42)
+		Me.ccb_Customers.MinimumSize = New System.Drawing.Size(200, 42)
+		Me.ccb_Customers.Name = "ccb_Customers"
+		Me.ccb_Customers.SelectedIndex = -1
+		Me.ccb_Customers.SelectedItem = Nothing
+		Me.ccb_Customers.SelectedValue = Nothing
+		Me.ccb_Customers.Size = New System.Drawing.Size(200, 42)
+		Me.ccb_Customers.TabIndex = 2
+		'
+		'pcb_Items
 		'
 		Me.pcb_Items.AutoSize = True
-		Me.pcb_Items.Location = New System.Drawing.Point(110, 66)
+		Me.pcb_Items.Location = New System.Drawing.Point(42, 76)
 		Me.pcb_Items.MaximumSize = New System.Drawing.Size(0, 42)
 		Me.pcb_Items.MinimumSize = New System.Drawing.Size(200, 42)
-		Me.pcb_Items.Name = "ProductsComboBox1"
+		Me.pcb_Items.Name = "pcb_Items"
 		Me.pcb_Items.SelectedIndex = -1
 		Me.pcb_Items.SelectedItem = Nothing
 		Me.pcb_Items.SelectedValue = Nothing
 		Me.pcb_Items.Size = New System.Drawing.Size(200, 42)
 		Me.pcb_Items.TabIndex = 1
 		'
+		'QuantityNudCtrl1
+		'
+		Me.QuantityNudCtrl1.Location = New System.Drawing.Point(42, 124)
+		Me.QuantityNudCtrl1.MaximumSize = New System.Drawing.Size(0, 42)
+		Me.QuantityNudCtrl1.MinimumSize = New System.Drawing.Size(100, 42)
+		Me.QuantityNudCtrl1.Name = "QuantityNudCtrl1"
+		Me.QuantityNudCtrl1.Quantity = 1
+		Me.QuantityNudCtrl1.Size = New System.Drawing.Size(100, 42)
+		Me.QuantityNudCtrl1.TabIndex = 3
+		'
+		'lbl_Total
+		'
+		Me.lbl_Total.AutoSize = True
+		Me.lbl_Total.Location = New System.Drawing.Point(75, 258)
+		Me.lbl_Total.Name = "lbl_Total"
+		Me.lbl_Total.Size = New System.Drawing.Size(31, 13)
+		Me.lbl_Total.TabIndex = 4
+		Me.lbl_Total.Text = "Total"
+		'
+		'txt_Total
+		'
+		Me.txt_Total.Location = New System.Drawing.Point(118, 255)
+		Me.txt_Total.Name = "txt_Total"
+		Me.txt_Total.ReadOnly = True
+		Me.txt_Total.Size = New System.Drawing.Size(100, 20)
+		Me.txt_Total.TabIndex = 6
+		'
+		'btn_AddCart
+		'
+		Me.btn_AddCart.Location = New System.Drawing.Point(151, 133)
+		Me.btn_AddCart.Name = "btn_AddCart"
+		Me.btn_AddCart.Size = New System.Drawing.Size(75, 23)
+		Me.btn_AddCart.TabIndex = 7
+		Me.btn_AddCart.Text = "Add to Cart"
+		Me.btn_AddCart.UseVisualStyleBackColor = True
+		'
+		'OrdersDatabase1
+		'
+		Me.OrdersDatabase1.InitialCatalog = "Media Ministry Test"
+		Me.OrdersDatabase1.Password = "M3AppPassword2499"
+		Me.OrdersDatabase1.Username = "M3App"
+		'
 		'PlaceOrderDialog
 		'
-		Me.AcceptButton = Me.OK_Button
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.CancelButton = Me.Cancel_Button
-		Me.ClientSize = New System.Drawing.Size(435, 315)
+		Me.ClientSize = New System.Drawing.Size(306, 353)
+		Me.Controls.Add(Me.btn_AddCart)
+		Me.Controls.Add(Me.txt_Total)
+		Me.Controls.Add(Me.lbl_Total)
+		Me.Controls.Add(Me.QuantityNudCtrl1)
+		Me.Controls.Add(Me.ccb_Customers)
 		Me.Controls.Add(Me.pcb_Items)
 		Me.Controls.Add(Me.TableLayoutPanel1)
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
@@ -98,7 +163,14 @@ Partial Class PlaceOrderDialog
 
 	End Sub
 	Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
-	Friend WithEvents OK_Button As System.Windows.Forms.Button
-	Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+	Friend WithEvents btn_Checkout As System.Windows.Forms.Button
+	Friend WithEvents btn_Cancel As System.Windows.Forms.Button
 	Friend WithEvents pcb_Items As ProductsComboBox
+	Friend WithEvents ccb_Customers As CustomersComboBox
+	Friend WithEvents QuantityNudCtrl1 As QuantityNudCtrl
+	Friend WithEvents lbl_Total As Windows.Forms.Label
+	Friend WithEvents txt_Total As Windows.Forms.TextBox
+	Friend WithEvents btn_AddCart As Windows.Forms.Button
+	Friend WithEvents OrdersDatabase1 As Database.OrdersDatabase
+	Friend WithEvents BackgroundWorker1 As ComponentModel.BackgroundWorker
 End Class
--- M3Tools/Dialogs/Orders/PlaceOrderDialog.vb
+++ M3Tools/Dialogs/Orders/PlaceOrderDialog.vb
@@ -1,18 +1,73 @@
-﻿Imports System.Windows.Forms
+﻿Imports System.Collections.ObjectModel
+Imports System.Windows.Forms
 
 Public Class PlaceOrderDialog
+	Private _cart As New Collection(Of CartItem)
 
-	Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+	Private ReadOnly Property Cart As Collection(Of CartItem)
+		Get
+			Return _cart
+		End Get
+	End Property
+
+	ReadOnly Property OrderTotal As Double
+		Get
+			Dim sum As Double = 0
+
+			For Each item In Cart
+				sum += (item.Item.Price * item.Quantity)
+			Next
+
+			Return sum
+		End Get
+	End Property
+
+	Private Sub Checkout(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Checkout.Click
 		Me.DialogResult = System.Windows.Forms.DialogResult.OK
 		Me.Close()
 	End Sub
 
-	Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+	Private Sub Cancel(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Cancel.Click
 		Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
 		Me.Close()
 	End Sub
 
 	Private Sub DialogLoading(sender As Object, e As EventArgs) Handles Me.Load
+		ccb_Customers.Reload()
 		pcb_Items.Reload()
+	End Sub
+
+	Private Sub AddToCart(sender As Object, e As EventArgs) Handles btn_AddCart.Click
+		Cart.Add(New CartItem(CType(pcb_Items.SelectedItem, Types.Product), QuantityNudCtrl1.Quantity))
+
+		' Update Order Total display
+		txt_Total.Text = String.Format("{0:C2}", OrderTotal)
+	End Sub
+End Class
+
+Friend Class CartItem
+	Private _item As Types.Product
+	Private _quantity As Integer
+
+	Property Item As Types.Product
+		Get
+			Return _item
+		End Get
+		Set(value As Types.Product)
+			_item = value
+		End Set
+	End Property
+	Property Quantity As Integer
+		Get
+			Return _quantity
+		End Get
+		Set(value As Integer)
+			_quantity = value
+		End Set
+	End Property
+
+	Public Sub New(item As Types.Product, quantity As Integer)
+		Me.Item = item
+		Me.Quantity = quantity
 	End Sub
 End Class
