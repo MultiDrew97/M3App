428ec59250fae5660cef9224beaa0a1518427a8f
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2024-07-16 17:15:29 -0500

    changing where info is coming from in the project

--- Media Ministry Manager/Forms/Management/InventoryManagement.cs
+++ Media Ministry Manager/Forms/Management/InventoryManagement.cs
@@ -32,7 +32,7 @@ namespace M3App
             UseWaitCursor = true;
 			bsInventory.DataSource = dbInventory.GetProducts();
 			bsInventory.ResetBindings(false);
-			ts_Tools.Count = string.Format(Properties.Resources.CountTemplate, idg_Inventory.Inventory.Count);
+			ts_Tools.Count = string.Format(Properties.Resources.COUNT_TEMPLATE, idg_Inventory.Inventory.Count);
 			UseWaitCursor = false;
         }
 
